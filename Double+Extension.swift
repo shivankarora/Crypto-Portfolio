ab33a024d03bf3dad4a49a9d3ae39a56ae3de712
Shivank Arora <63502234+shivankarora@users.noreply.github.com>
2022-02-07 17:35:26 +0530

    Create Double+Extension.swift

---
+++ Double+Extension.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+extension Double {
+    func converting(from input: ClosedRange<Self>, to output: ClosedRange<Self>) -> Self {
+        let x = (output.upperBound - output.lowerBound) * (self - input.lowerBound)
+        let y = (input.upperBound - input.lowerBound)
+        return x / y + output.lowerBound
+    }
+}
